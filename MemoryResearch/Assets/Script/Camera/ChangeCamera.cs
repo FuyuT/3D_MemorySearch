755941252d7522408d3981e7d88692f9a70d99b3
木下 <C0120@oic.jp>
2022-10-21 15:23:19 +0900

    α版ステージ完成？（マージ途中、バグあり）

--- MemoryResearch/Assets/Script/Camera/ChangeCamera.cs
+++ MemoryResearch/Assets/Script/Camera/ChangeCamera.cs
@@ -35,6 +35,7 @@ public class ChangeCamera : MonoBehaviour
         //スペースキーが押すたびに、カメラを切り替える
         if (Input.GetKeyDown("space"))
         {
+                Debug.Log("通った");
             if (MainCamera.activeSelf)
             {
                 //サブカメラをアクティブに設定
