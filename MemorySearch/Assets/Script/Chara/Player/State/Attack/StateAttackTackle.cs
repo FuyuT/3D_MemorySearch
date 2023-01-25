b1aad940fa98b6a79c557c94af53dc8d0c5e832e
Fuyu <fuyu2363@gmail.com>
2023-01-26 05:46:28 +0900

    ■マージ修正
    ■ライトのバグ修正
    LightingウィンドウのAutoGenerateが原因で色が変わるバグが発生していたので、
    LightSettingを作成し、AutoGenerateを使用しないように変更
    変更する場合は手動で行わなければ反映されないので注意が必要
    公式リファレンス
    https://docs.unity3d.com/ja/2018.4/Manual/GlobalIllumination.html

---
+++ MemorySearch/Assets/Script/Chara/Player/State/Attack/StateAttackTackle.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using State = MyUtil.PlayerState;
+
+/// <summary>
+/// タックル
+/// </summary>
+public class StateAttackTackle : State
+{
+    Vector3 tackleVelocity;
+    Vector3 accelerateionVec;
+    float   nowTackleTime;
+    bool    isMove;
+
+
+    protected override void OnEnter(MyUtil.ActorState<Player> prevState)
+    {
+        //アニメーションの更新
+        BehaviorAnimation.UpdateTrigger(ref Owner.animator, "Attack_Tackle");
+
+        //ダッシュベクトルを作成
+        tackleVelocity = BehaviorMoveToInput.GetDushVec(Owner.transform.forward);
+
+        //加速値を作成
+        accelerateionVec = tackleVelocity * Owner.TackleAcceleration;
+
+        //初速を設定
+        tackleVelocity *= Owner.TackleStartSpeed;
+
+        //nowTackleTime
+        nowTackleTime = Owner.TackleTime;
+
+        //攻撃力設定
+        Owner.SetAttackPower(8);
+
+        isMove = false;
+
+        //SE
+        SoundManager.instance.PlaySe(Owner.TackleSE, Owner.transform.position);
+    }
+
+    protected override void OnUpdate()
+    {
+        //タックルの移動中で無ければ終了
+        if (!Owner.animator.GetCurrentAnimatorStateInfo(0).IsName("Tackle_Move_Start")
+            && !Owner.animator.GetCurrentAnimatorStateInfo(0).IsName("Tackle_Move"))
+        {
+            return;
+        }
+        else if(BehaviorAnimation.IsName(ref Owner.animator, "Tackle_Move_Start"))
+        {
+            if (!isMove)
+            {
+                Owner.effectWind.Play();
+                isMove = true;
+            }
+        }
+        //目標地点まで毎フレーム移動
+        if (nowTackleTime > 0)
+        {
+            nowTackleTime -= Time.deltaTime;
+
+            tackleVelocity += accelerateionVec;
+
+            //スピード設定
+            Actor.IVelocity().SetVelocity(tackleVelocity);
+
+            Owner.effectWind.transform.position = Owner.transform.position;
+        }
+
+        SelectNextState();
+    }
+
+    protected override void SelectNextState()
+    {
+        //移動が終了していたら待機へ
+        if (nowTackleTime < 0)
+        {
+            stateMachine.Dispatch((int)Player.State.Idle);
+            return;
+        }
+
+        //装備から状態を選択
+        EquipmentSelectNextState();
+    }
+
+    protected override void OnExit(MyUtil.ActorState<Player> nextState)
+    {
+        Owner.nowTackleDelayTime = Owner.TackleDelayTime;
+
+        Owner.SetAttackPower(0);
+
+        Owner.animator.ResetTrigger("Attack_Tackle");
+
+        Owner.effectWind.Stop();
+
+        SoundManager.instance.StopSe(Owner.TackleSE);
+    }
+}
---
+++ MemorySearch/Assets/Script/DataScript/Manager/DataManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    /*******************************
+    * private
+    *******************************/
+    [SerializeField] TextAsset memoryDataTxt;
+    [SerializeField] TextAsset enemyDataTxt;
+
+    [Header("memorySpriteには、メモリの画像を「Memory.cs MemoryType」の順番に配置してください。")]
+    [SerializeField] Sprite[] memorySprite;
+
+    MemoryData memoryData;
+    PlayerData playerData;
+    EnemyData  enemyData;
+    OptionData optionData;
+
+    string saveDataPath;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            memoryData = new MemoryData();
+            playerData = new PlayerData();
+            enemyData  = new EnemyData();
+            optionData = new OptionData();
+            Load();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Load()
+    {
+        //メモリーデータの読み込み
+        memoryData.Load(memoryDataTxt);
+
+        //セーブデータパスの取得
+        saveDataPath = SaveDataProcess.GetSaveDataPath();
+        //セーブデータの読み込み
+        SaveDataProcess.Load(saveDataPath, ref playerData, ref optionData);
+
+        //セーブデータ反映
+        //サウンド
+        try
+        {
+            SoundManager soundManager = GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>();
+            soundManager.BgmVolume = optionData.soundOption.bgmVolume;
+            soundManager.SeVolume = optionData.soundOption.seVolume;
+        }
+        catch
+        {
+            Debug.Log("SoundManagerが見つかりません");
+        }
+        //明るさ
+    }
+
+    /*******************************
+    * public
+    *******************************/
+    public static DataManager instance;
+
+    //メモリデータのインターフェイスを取得
+    public IMemoryData IMemoryData()
+    {
+        return memoryData;
+    }
+
+    //プレイヤーデータのインターフェイスを取得
+    public IPlayerData IPlayerData()
+    {
+        return playerData;
+    }
+
+    //オプションデータのインターフェイスを取得
+    public IOptionData IOptionData()
+    {
+        return optionData;
+    }
+
+    //データをセーブする
+    public void Save()
+    {
+        SaveDataProcess.Save(saveDataPath, ref playerData, ref optionData);
+    }
+
+    //メモリの画像を取得
+    public Sprite GetMemorySprite(MemoryType type)
+    {
+        try
+        {
+            return memorySprite[(int)type];
+        }
+        catch
+        {
+            //配列外の数字が来たら0番の要素を返す
+            return memorySprite[0];
+        }
+    }
+}
